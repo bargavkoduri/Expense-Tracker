0932cae15a58dfd1d57a50c95200f8e163602b83
Bargav Koduri <bhargavkoduri123@gmail.com>
2024-10-03 12:53:25 +0530

    signin/signup bug fix

--- frontend/src/pages/signin.js
+++ frontend/src/pages/signin.js
@@ -3,7 +3,7 @@ import { SignIn } from "@clerk/clerk-react";
 export default function SignInPage() {
     return  (
     <div className="flex justify-center mt-11">
-        <SignIn path="/sign-in" forceRedirectUrl={"/dashboard"} signUpUrl="/sign-up"/>
+        <SignIn forceRedirectUrl={"/dashboard"} signUpUrl="/sign-up"/>
     </div>
     )
 }
--- frontend/src/pages/signup.js
+++ frontend/src/pages/signup.js
@@ -3,7 +3,7 @@ import { SignUp } from '@clerk/clerk-react'
 export default function SignUpPage() {
     return (
         <div className="flex justify-center mt-11">
-            <SignUp path="/sign-up" forceRedirectUrl={"/dashboard"} signInUrl='/sign-in'/>
+            <SignUp forceRedirectUrl={"/dashboard"} signInUrl='/sign-in'/>
         </div>
     )
 }
